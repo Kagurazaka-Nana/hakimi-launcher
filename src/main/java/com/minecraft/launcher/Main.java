61e9841469de0c04175055d585078bb6a1c01baf
Kagurazaka-Nana <nightsky_chaser@outlook.com>
2026-08-27 21:17:31 +0800

    refactor(launcher): enable manifest download and save latest release client.json in launcherTest layout
    Signed-off-by: Kagurazaka-Nana <nightsky_chaser@outlook.com>

--- src/main/java/com/minecraft/launcher/Main.java
+++ src/main/java/com/minecraft/launcher/Main.java
@@ -3,62 +3,66 @@ package com.minecraft.launcher;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.minecraft.launcher.model.manifest.VersionInfo;
 import com.minecraft.launcher.model.manifest.VersionManifest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-    private final String VERSIONS_MANIFEST_URL =
+    private static final String VERSIONS_MANIFEST_URL =
             "https://piston-meta.mojang.com/mc/game/version_manifest.json";
-    private final String CUSTOM_USER_AGENT =
+    private static final String CUSTOM_USER_AGENT =
             "MinecraftLauncher/1.0 (Java 21)";
 
-    private final Path TEMP_DIR = Path.of("./temp");
-    private final Path VERSIONS_MANIFEST_JSON_PATH = TEMP_DIR.resolve("version_manifest.json");
+    private static final Path TEMP_DIR =
+            Path.of("./temp");
+    private static final Path TEST_DIR =
+            Path.of("./launcherTest/.minecraft");
+    private static final Path ASSETS_DIR =
+            TEST_DIR.resolve( "assets");
+    private static final Path LIBRARIES_DIR =
+            TEST_DIR.resolve( "libraries");
+    private static final Path VERSIONS_DIR =
+            TEST_DIR.resolve( "versions");
+
+    private static final Path VERSIONS_MANIFEST_JSON_PATH =
+            TEMP_DIR.resolve("version_manifest.json");
+    private static final Logger logger =
+            LoggerFactory.getLogger(Main.class.getName());
+    private static final HttpDownloader downloader =
+            new HttpDownloader();
 
     public static void main(String[] args) throws Exception {
-        Main app = new Main();
-        HttpDownloader downloader = new HttpDownloader();
 
-        System.out.println("Hello My Hakimi Launcher");
-        System.out.println("Current Configurations:");
-        System.out.println("VERSIONS_MANIFEST_URL: " + app.VERSIONS_MANIFEST_URL);
-        System.out.println("CUSTOM_USER_AGENT: " + app.CUSTOM_USER_AGENT);
+        logger.info("Hello My Hakimi Launcher");
+        logger.info("Current Configurations:");
+        logger.info("VERSIONS_MANIFEST_URL: " + VERSIONS_MANIFEST_URL);
+        logger.info("CUSTOM_USER_AGENT: " + CUSTOM_USER_AGENT);
 
-        System.out.println("Start download and parse versions_manifest.json");
-        // Files.createDirectories(app.VERSIONS_MANIFEST_JSON_PATH.getParent());
-        // downloader.downloadFile(app.VERSIONS_MANIFEST_URL, app.VERSIONS_MANIFEST_PATH);
-        // System.out.println("Done.");
+        logger.info("Start download and parse versions_manifest.json");
+        Files.createDirectories(VERSIONS_MANIFEST_JSON_PATH.getParent());
+        downloader.downloadFile(VERSIONS_MANIFEST_URL, VERSIONS_MANIFEST_JSON_PATH);
+        logger.info("Done.");
+
         ObjectMapper mapper = new ObjectMapper();
-        VersionManifest manifest = mapper.readValue(app.VERSIONS_MANIFEST_JSON_PATH.toFile(), VersionManifest.class);
+        VersionManifest manifest = mapper.readValue(VERSIONS_MANIFEST_JSON_PATH.toFile(), VersionManifest.class);
 
-        System.out.println("Start download client.json of the latest release");
+        logger.info("Start download client.json of the latest release");
+        downloadLatestReleaseMeta(manifest);
+
+    }
+
+    private static void downloadLatestReleaseMeta(VersionManifest manifest) throws IOException, InterruptedException {
         VersionInfo latestRelease = manifest.getLatestRelease()
                 .orElseThrow(() -> new IllegalStateException("Manifest 中未找到最新 release 版本信息"));
-        Path latestReleaseClientJsonPath = app.TEMP_DIR.resolve(latestRelease.getId() + ".json");
+
+        String id = latestRelease.getId();
+        Path latestReleasePath = VERSIONS_DIR.resolve(id);
+        Files.createDirectories(latestReleasePath);
+        Path latestReleaseClientJsonPath = latestReleasePath.resolve(id + ".json");
         downloader.downloadFile(latestRelease.getUrl(), latestReleaseClientJsonPath);
-
-        //JsonUtils.printPretty(latestRelease);
-
-
-
-
-        // 现在可以正确获取版本列表
-
-//        System.out.println("最新的发行版: " + manifest.getLatest().getRelease());
-//        System.out.println("最新的快照: " + manifest.getLatest().getSnapshot());
-//        System.out.println("版本数量: " + manifest.getVersions().size());
-//        for (VersionInfo info : manifest.getVersions()) {
-//            System.out.println("版本: ");
-//            System.out.println("{");
-//            System.out.println("id: " + info.getId());
-//            System.out.println("type: " + info.getType());
-//            System.out.println("url: " + info.getUrl());
-//            System.out.println("time: " + info.getTime());
-//            System.out.println("releaseTime: " + info.getReleaseTime());
-//            System.out.println("sha1: " + info.getSha1());
-//            System.out.println("complianceLevel: " + info.getComplianceLevel());
-//            System.out.println("}");
-//        }
     }
 }
 
